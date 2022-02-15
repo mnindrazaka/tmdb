9f642a879eb8eff438a612ee0aa103c16ba26091
AndriP8 <andrikcoc26@gmail.com>
2022-02-15 07:52:05 +0700

    chore : update config test image & styling

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,12 @@
+import Image from "./asset/resource/logo.png";
+import "./app.css";
+
 const App = () => {
   return (
     <div>
       <h1>hello world</h1>
+      <img src={Image} alt="" />
+      <button className="btn layout">Submit</button>
     </div>
   );
 };
