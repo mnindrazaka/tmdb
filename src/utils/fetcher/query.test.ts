f5763b286b7ab443b41705fd3fd577b857059d4a
M Nindra Zaka <mnindrazaka@gmail.com>
2022-02-21 21:17:48 +0700

    feat: implement refetch after success or error (#9)

--- src/utils/fetcher/query.test.ts
+++ src/utils/fetcher/query.test.ts
@@ -52,3 +52,43 @@ it("should not trigger fetch automatically when lazy is turned on", async () => 
 
   expect(result.current.state).toEqual({ tag: "success", data });
 });
+
+it("can trigger fetch after fetching has been success", async () => {
+  const data = ["aka", "andri"];
+  const fetchData = () =>
+    new Promise<string[]>((resolve) => setTimeout(() => resolve(data), 1000));
+
+  const { result, waitForNextUpdate } = renderHook(() =>
+    useQuery<string[]>("test", fetchData)
+  );
+
+  await waitForNextUpdate();
+  expect(result.current.state).toEqual({ tag: "success", data });
+
+  act(() => result.current.refetch());
+  expect(result.current.state).toEqual({ tag: "loading" });
+
+  await waitForNextUpdate();
+  expect(result.current.state).toEqual({ tag: "success", data });
+});
+
+it("can trigger fetch after fetching has been error", async () => {
+  const error = new Error("something went wrong");
+  const fetchData = () =>
+    new Promise<[]>((_resolve, reject) =>
+      setTimeout(() => reject(error), 1000)
+    );
+
+  const { result, waitForNextUpdate } = renderHook(() =>
+    useQuery<string[]>("test", fetchData)
+  );
+
+  await waitForNextUpdate();
+  expect(result.current.state).toEqual({ tag: "error", error });
+
+  act(() => result.current.refetch());
+  expect(result.current.state).toEqual({ tag: "loading" });
+
+  await waitForNextUpdate();
+  expect(result.current.state).toEqual({ tag: "error", error });
+});
