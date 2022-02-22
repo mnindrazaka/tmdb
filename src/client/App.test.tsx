cdb58eecd7ab5aacc5807a3e3f0fc74b19a2ecbd
Andri purnomo <56879512+AndriP8@users.noreply.github.com>
2022-02-22 18:13:53 +0700

    chore: introduce mockapi (#10)
    * update setup branch github actions
    
    * chore: introduce mockapi

--- src/client/App.test.tsx
+++ src/client/App.test.tsx
@@ -1,8 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import App from "./App";
 
-test("App should have a heading.", () => {
+const renderApp = async () => {
   render(<App />);
+  await waitForElementToBeRemoved(() => screen.getByText("loading..."));
+};
+
+test("App should have a heading.", async () => {
+  await renderApp();
   const header = screen.getByRole("heading", { name: "hello world" });
   expect(header).toBeInTheDocument();
+});
+
+test("Should render list of name", async () => {
+  await renderApp();
+
+  const results = await screen.findByRole("heading", {
+    name: "Leanne Graham",
+  });
+
+  expect(results).toBeInTheDocument();
 });
