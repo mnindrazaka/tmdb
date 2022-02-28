2d06f8af2468fa9e9b0d0c97a3910fab7b60a49b
AndriP8 <andrikcoc26@gmail.com>
2022-02-28 09:15:08 +0700

    chore: introduce chakra-ui

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "./asset/resource/logo.png";
 import { usersApi, User } from "../utils/fetcher";
 import "./app.css";
+import { Heading, HStack, Text } from "@chakra-ui/react";
 
 const App = () => {
   const [loading, setLoading] = React.useState(false);
@@ -20,9 +21,12 @@ const App = () => {
       {loading ? (
         <p>loading...</p>
       ) : (
-        users.map((user) => <h1 key={user.id}>{user.name}</h1>)
+        users.map((user) => (
+          <Heading as={"h1"} key={user.id}>
+            {user.name}
+          </Heading>
+        ))
       )}
-
       <h1>hello world</h1>
       <img src={Image} alt="react logo" />
       <button className="btn layout">Submit</button>
