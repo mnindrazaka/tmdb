59b6c251b919fef72cc75ba96ab4bc97bd6b924c
AndriP8 <andrikcoc26@gmail.com>
2022-03-05 10:50:34 +0700

    chore: update schema to be more descriptive

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import { Movie, moviesApi } from "@/utils/fetcher";
 
 const HomePage = () => {
   const [loading, setLoading] = React.useState(false);
-  const [movies, setMovies] = React.useState<Movie[]>();
+  const [movies, setMovies] = React.useState<Movie[]>([]);
 
   React.useEffect(() => {
     setLoading(true);
     moviesApi
-      .getMovies({ apiKey: "11dfe233fe073aab1aaa3389310e3358" })
+      .getMoviePopular({ apiKey: "11dfe233fe073aab1aaa3389310e3358" })
       .then((res) => {
         setMovies(res.results);
         setLoading(false);
@@ -21,7 +21,7 @@ const HomePage = () => {
       {loading ? (
         <p>loading...</p>
       ) : (
-        movies?.map((movie) => (
+        movies.map((movie) => (
           <Heading as={"h1"} key={movie.id}>
             {movie.originalTitle}
           </Heading>
