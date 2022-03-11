56664e2b031e9b93c37c596d88a5bba487409aed
AndriP8 <andrikcoc26@gmail.com>
2022-03-11 21:20:18 +0700

    feat: added movieSlider main, loading & error variant

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,12 +2,10 @@ import {
   Box,
   CircularProgress,
   CircularProgressLabel,
-  Grid,
   Heading,
   Image,
   Text,
   VStack,
-  GridItem,
   Flex,
   BoxProps,
   Stack,
@@ -54,38 +52,36 @@ const MovieCard = (props: MovieCardProps) => {
           </Stack>
         </Box>
       ) : (
-        <Grid templateColumns="repeat(1, 1fr)" gap={6}>
-          <GridItem rounded={8} boxShadow={"base"}>
-            <Image src={props.posterPath} h={"52"} w={"full"} roundedTop={8} />
-            <CircularProgress
-              value={props.voteCount / 100}
-              color="green.500"
-              ml={2}
-              position={"relative"}
-              bottom={"6"}
-              right={0}
-              textColor={"white"}
-              backgroundColor="gray.900"
-              borderRadius={"full"}
-            >
-              <CircularProgressLabel>
-                {Math.round(props.voteCount / 100)}%
-              </CircularProgressLabel>
-            </CircularProgress>
-            <VStack spacing={0} p={4} alignItems={"start"} mt={-4}>
-              <Heading as={"h6"} size={"sm"}>
-                {props.title}
-              </Heading>
-              <Text marginTop={8} color={"gray.700"}>
-                {new Intl.DateTimeFormat("en-US", {
-                  day: "2-digit",
-                  month: "short",
-                  year: "numeric",
-                }).format(new Date(props.releaseDate))}
-              </Text>
-            </VStack>
-          </GridItem>
-        </Grid>
+        <Box rounded={8} boxShadow={"base"}>
+          <Image src={props.posterPath} h={"52"} w={"full"} roundedTop={8} />
+          <CircularProgress
+            value={props.voteCount / 100}
+            color="green.500"
+            ml={2}
+            position={"relative"}
+            bottom={"6"}
+            right={0}
+            textColor={"white"}
+            backgroundColor="gray.900"
+            borderRadius={"full"}
+          >
+            <CircularProgressLabel>
+              {Math.round(props.voteCount / 100)}%
+            </CircularProgressLabel>
+          </CircularProgress>
+          <VStack spacing={0} p={4} alignItems={"start"} mt={-4}>
+            <Heading as={"h6"} size={"sm"}>
+              {props.title}
+            </Heading>
+            <Text marginTop={8} color={"gray.700"}>
+              {new Intl.DateTimeFormat("en-US", {
+                day: "2-digit",
+                month: "short",
+                year: "numeric",
+              }).format(new Date(props.releaseDate))}
+            </Text>
+          </VStack>
+        </Box>
       )}
     </Box>
   );
---
+++ src/components/MovieSlider.tsx
@@ -0,0 +1,151 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Stack,
+  Tab,
+  TabList,
+  Tabs,
+} from "@chakra-ui/react";
+import MovieCard from "@/components/MovieCard";
+import React from "react";
+
+interface TabsOptions {
+  key: string;
+  title: string;
+}
+
+interface HeadingMovieSliderProps {
+  title: string;
+  tabOptions: TabsOptions[];
+  onTabChange: (index: number) => void;
+}
+
+const HeadingMovieSlider = ({
+  title,
+  tabOptions,
+  onTabChange,
+}: HeadingMovieSliderProps) => {
+  return (
+    <Stack
+      p={6}
+      flexDirection={["column", "row"]}
+      alignItems={["start", "center"]}
+      spacing={[4, 0]}
+    >
+      <Heading as={"h6"} fontSize={"2xl"} mr={4}>
+        {title}
+      </Heading>
+      <Tabs
+        variant="soft-rounded"
+        colorScheme="blue"
+        borderRadius={"full"}
+        border={"1px"}
+        height={"8"}
+        onChange={(index) => onTabChange(index)}
+      >
+        <TabList height={"full"}>
+          {tabOptions.map((option) => (
+            <Tab key={option.key} value={option.key} color={"gray.800"}>
+              {option.title}
+            </Tab>
+          ))}
+        </TabList>
+      </Tabs>
+    </Stack>
+  );
+};
+
+interface Movie {
+  id: number;
+  posterPath: string;
+  title: string;
+  releaseDate: string;
+  voteCount: number;
+}
+
+type MovieSliderProps =
+  | { state: "loading"; tabOptions: TabsOptions[]; title: string }
+  | {
+      state: "error";
+      message: string;
+      tabOptions: TabsOptions[];
+      title: string;
+    }
+  | {
+      state: "loaded";
+      movies: Record<string, Movie[]>;
+      tabOptions: TabsOptions[];
+      title: string;
+    };
+
+const MovieSlider = (props: MovieSliderProps) => {
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = React.useState(0);
+
+  if (props.state === "loading") {
+    return (
+      <Box>
+        <HeadingMovieSlider
+          title={props.title}
+          tabOptions={props.tabOptions}
+          onTabChange={setSelectedCategoryIndex}
+        />
+        <Flex overflowX={"scroll"} pb={8}>
+          <MovieCard isLoading mx={3} flex={1} />
+          <MovieCard isLoading mx={3} flex={1} />
+          <MovieCard isLoading mx={3} flex={1} />
+          <MovieCard isLoading mx={3} flex={1} />
+          <MovieCard isLoading mx={3} flex={1} />
+          <MovieCard isLoading mx={3} flex={1} />
+          <MovieCard isLoading mx={3} flex={1} />
+        </Flex>
+      </Box>
+    );
+  }
+
+  if (props.state === "loaded") {
+    return (
+      <Box>
+        <HeadingMovieSlider
+          title={props.title}
+          tabOptions={props.tabOptions}
+          onTabChange={setSelectedCategoryIndex}
+        />
+        <Flex overflowX={"scroll"} pb={12}>
+          {props.movies[props.tabOptions[selectedCategoryIndex].key].map(
+            (movie) => (
+              <MovieCard
+                key={movie.id}
+                posterPath={movie.posterPath}
+                title={movie.title}
+                releaseDate={movie.releaseDate}
+                voteCount={movie.voteCount}
+                mx={3}
+                flexBasis={"300px"}
+              />
+            )
+          )}
+        </Flex>
+      </Box>
+    );
+  }
+
+  if (props.state === "error") {
+    return (
+      <Box>
+        <HeadingMovieSlider
+          title={props.title}
+          tabOptions={props.tabOptions}
+          onTabChange={setSelectedCategoryIndex}
+        />
+        <Heading as={"h4"} size={"sm"} mx={6} pb={12}>
+          {props.message}
+        </Heading>
+      </Box>
+    );
+  }
+
+  return null;
+};
+
+export default MovieSlider;
