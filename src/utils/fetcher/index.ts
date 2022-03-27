b8aca782459fd848326ff6720df6212ab8093d31
AndriP8 <andrikcoc26@gmail.com>
2022-03-27 08:56:19 +0700

    chore: update import path

--- src/utils/fetcher/index.ts
+++ src/utils/fetcher/index.ts
@@ -1,6 +1,12 @@
-import { Configuration, MoviesApi } from "@/__generated__/api";
+{
+  /*
+  We need change absolute import path to general import, because when run test webpack can't detect folder /__generated__/
+*/
+}
 
-export * from "@/__generated__/api";
+import { Configuration, MoviesApi } from "../../__generated__/api";
+
+export * from "../../__generated__/api";
 
 const configuration = new Configuration({
   basePath: "https://api.themoviedb.org/3"
