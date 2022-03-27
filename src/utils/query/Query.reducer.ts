5f9c42300275e26888040c2a743e320e1e87ed8b
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-03-27 07:29:27 +0700

    refactor: use pattern maching on query reducer

--- src/utils/query/Query.reducer.ts
+++ src/utils/query/Query.reducer.ts
@@ -1,5 +1,6 @@
 import * as Query from "@/utils/query/Query";
 import * as QueryContext from "@/utils/query/Query.context";
+import { match } from "ts-pattern";
 
 export namespace State {
   export type t<T> =
@@ -14,26 +15,18 @@ export namespace State {
     callback: Query.Callback.t<T>,
     config: Query.Config.t
   ) => {
-    switch (state.tag) {
-      case "idle": {
+    return match<t<T>, void>(state)
+      .with({ tag: "idle" }, () => {
         if (!config.lazy) dispatch({ tag: "fetch" });
-        break;
-      }
-      case "loading": {
+      })
+      .with({ tag: "loading" }, () => {
         callback()
           .then((data) => dispatch({ tag: "fetchSuccess", data }))
           .catch((error) => dispatch({ tag: "fetchError", error }));
-        break;
-      }
-      case "success":
-      case "error": {
-        break;
-      }
-      default: {
-        const exhaustiveCheck: never = state;
-        return exhaustiveCheck;
-      }
-    }
+      })
+      .with({ tag: "success" }, () => {})
+      .with({ tag: "error" }, () => {})
+      .exhaustive();
   };
 
   export const make = <T>(store: QueryContext.Store.t, key: string): t<T> => {
@@ -52,72 +45,24 @@ export namespace Action {
 export const make =
   <T>() =>
   (state: State.t<T>, action: Action.t<T>): State.t<T> => {
-    switch (state.tag) {
-      case "idle": {
-        switch (action.tag) {
-          case "fetch": {
-            return { tag: "loading" };
-          }
-          case "fetchSuccess":
-          case "fetchError": {
-            return state;
-          }
-          default: {
-            const exhaustiveCheck: never = action;
-            return exhaustiveCheck;
-          }
-        }
-      }
-      case "loading": {
-        switch (action.tag) {
-          case "fetch": {
-            return state;
-          }
-          case "fetchSuccess": {
-            return { tag: "success", data: action.data };
-          }
-          case "fetchError": {
-            return { tag: "error", error: action.error };
-          }
-          default: {
-            const exhaustiveCheck: never = action;
-            return exhaustiveCheck;
-          }
-        }
-      }
-      case "success": {
-        switch (action.tag) {
-          case "fetch": {
-            return { tag: "loading" };
-          }
-          case "fetchSuccess":
-          case "fetchError": {
-            return state;
-          }
-          default: {
-            const exhaustiveCheck: never = action;
-            return exhaustiveCheck;
-          }
-        }
-      }
-      case "error": {
-        switch (action.tag) {
-          case "fetch": {
-            return { tag: "loading" };
-          }
-          case "fetchSuccess":
-          case "fetchError": {
-            return state;
-          }
-          default: {
-            const exhaustiveCheck: never = action;
-            return exhaustiveCheck;
-          }
-        }
-      }
-      default: {
-        const exhaustiveCheck: never = state;
-        return exhaustiveCheck;
-      }
-    }
+    return match<[State.t<T>, Action.t<T>], State.t<T>>([state, action])
+      .with([{ tag: "idle" }, { tag: "fetch" }], () => ({ tag: "loading" }))
+      .with([{ tag: "idle" }, { tag: "fetchSuccess" }], () => state)
+      .with([{ tag: "idle" }, { tag: "fetchError" }], () => state)
+      .with([{ tag: "loading" }, { tag: "fetch" }], () => state)
+      .with([{ tag: "loading" }, { tag: "fetchSuccess" }], ([_, action]) => ({
+        tag: "success",
+        data: action.data
+      }))
+      .with([{ tag: "loading" }, { tag: "fetchError" }], ([_, action]) => ({
+        tag: "error",
+        error: action.error
+      }))
+      .with([{ tag: "success" }, { tag: "fetch" }], () => ({ tag: "loading" }))
+      .with([{ tag: "success" }, { tag: "fetchSuccess" }], () => state)
+      .with([{ tag: "success" }, { tag: "fetchError" }], () => state)
+      .with([{ tag: "error" }, { tag: "fetch" }], () => ({ tag: "loading" }))
+      .with([{ tag: "error" }, { tag: "fetchSuccess" }], () => state)
+      .with([{ tag: "error" }, { tag: "fetchError" }], () => state)
+      .exhaustive();
   };
